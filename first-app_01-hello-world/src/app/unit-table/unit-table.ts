5bac03d3232eac912fdcdb873497662a1bc4145e
AdriSanPedro <asanpedro@alu.ucam.edu>
2025-11-20 16:17:39 +0100

    cambiar tabla si hay dormitorios o terrazas

--- first-app_01-hello-world/src/app/unit-table/unit-table.ts
+++ first-app_01-hello-world/src/app/unit-table/unit-table.ts
@@ -1,15 +1,28 @@
-import { Component, input } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Unit } from '../unit';
+import { Component, computed, input } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Unit } from "../unit";
 
 @Component({
-  selector: 'app-unit-table',
+  selector: "app-unit-table",
   standalone: true,
   imports: [CommonModule],
   templateUrl: "./unit-table.html",
-  styleUrl: './unit-table.css',
+  styleUrl: "./unit-table.css",
 })
 export class UnitTableComponent {
   // Recibe la lista de unidades
   units = input.required<Unit[]>();
+
+  // NUEVO: Un 'signal' calculado que determina qué columna mostrar
+  // Revisa el primer piso de la lista para ver si tiene la propiedad 'terrazas' o 'dormitorios'.
+  public displayColumn = computed(() => {
+    const firstUnit = this.units()?.[0];
+    if (firstUnit && typeof firstUnit.terrazas !== "undefined") {
+      return "terraza";
+    }
+    else if (firstUnit && typeof firstUnit.dormitorios !== "undefined") {
+      return "dormitorios";
+    }
+    return "none"; // Por si acaso no viene ninguna de las dos
+  });
 }
