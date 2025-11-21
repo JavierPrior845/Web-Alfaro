a5a419a61d1145a26bd01b045178b6ff266ade3d
AdriSanPedro <asanpedro@alu.ucam.edu>
2025-11-21 13:29:59 +0100

    colores nuevos y menu

--- first-app_01-hello-world/src/app/app.ts
+++ first-app_01-hello-world/src/app/app.ts
@@ -6,8 +6,9 @@ import { FooterContact } from "./footer-contact/footer-contact";
   selector: "app-root",
   imports: [RouterModule, FooterContact],
   template: `
-      <main>
+    <main>
       <header class="top-bar">
+        <!-- LOGO -->
         <a [routerLink]="['/']" class="brand-name">
           <img
             class="brand-logo"
@@ -16,10 +17,34 @@ import { FooterContact } from "./footer-contact/footer-contact";
             aria-hidden="true"
           />
         </a>
+
+        <!-- MENÚ -->
+        <nav class="menu">
+          <a routerLink="/inicio" routerLinkActive="active">INICIO</a>
+          <a routerLink="/trabajos" routerLinkActive="active">TRABAJOS</a>
+          <a routerLink="/colaboraciones" routerLinkActive="active"
+            >COLABORACIONES</a
+          >
+          <a routerLink="/en-venta" routerLinkActive="active">EN VENTA</a>
+          <a routerLink="/proyectos" routerLinkActive="active">PROYECTOS</a>
+        </nav>
         <div class="contact-info">
           <a href="tel:+34968355053" class="phone-number">
-            <svg class="phone-icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-              <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
+            <svg
+              class="phone-icon"
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path
+                d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"
+              ></path>
             </svg>
             +34 968 355 053
           </a>
@@ -29,7 +54,7 @@ import { FooterContact } from "./footer-contact/footer-contact";
         <router-outlet></router-outlet>
       </section>
     </main>
-    <app-footer-contact></app-footer-contact>  
+    <app-footer-contact></app-footer-contact>
   `,
   styleUrls: ["./app.css"],
 })
