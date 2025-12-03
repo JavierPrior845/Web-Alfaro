be3517d343adca342348843965c849c3eb4cdbf9
AdrianSanPedroTorralba <adriasapedro758@gmail.com>
2025-12-03 16:09:45 +0100

    pdfs modificados, tablas y resume actualizadas

--- frontend/src/app/unit.ts
+++ frontend/src/app/unit.ts
@@ -1,10 +1,15 @@
 export interface Unit {
-  vivienda: string;
-  m2: number;
-  terrazas?: number | string; // Puede ser opcional o un guion
+  planta?: string;
+  tipologia?: string;
   dormitorios?: number | string; // Puede ser opcional o un guion
   banos?: number | string;
-  trastero?: string;
+  piscina?: string;
+  m2construidos?: number | string;
+  m2utiles?: number | string;
+  m2terraza?: number | string;
+  orientacion?:string;
+  eficEnergetica?: string;
   garage?: string;
+  trastero?: string;
   planoPdfUrl?: string;   // URL para descargar el plano (¡NUEVO CAMPO!)
 }
